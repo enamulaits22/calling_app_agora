1009b6d6900c845fd80c4ebdd8e5774c53ed204a
enamulaits22 <enamulaits22@gmail.com>
2022-06-19 11:25:40 +0600

    agora video call

---
+++ lib/config/config.dart
@@ -0,0 +1,6 @@
+class Config {
+  Config._();
+  static String APP_ID = '46f98f952b5f4a608f651d43fdf47cd9';
+  static String Token = '00646f98f952b5f4a608f651d43fdf47cd9IAA+SQKGiQStAZuvB6v2toUdQEMCrzPMpzwnfSXbyqb9sHG6sh0AAAAAEACXhJCZgvWvYgEAAQCC9a9i';
+  static String channelName = 'audacitycall';
+}
