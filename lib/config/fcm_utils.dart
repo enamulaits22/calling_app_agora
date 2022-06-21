8598d32218507b7fe860b5579a157245a7e70ae0
enamulaits22 <enamulaits22@gmail.com>
2022-06-21 11:00:29 +0600

    call using apiservice

--- lib/config/fcm_utils.dart
+++ lib/config/fcm_utils.dart
@@ -1,13 +1,17 @@
-import 'dart:math';
+
+import 'dart:developer';
+import 'dart:math' as math;
 
 import 'package:calling_app/calling_screen.dart';
+import 'package:calling_app/config/config.dart';
 import 'package:calling_app/main.dart';
+import 'package:connectycube_sdk/connectycube_sdk.dart';
 import 'package:flutter/material.dart';
 import 'package:connectycube_flutter_call_kit/connectycube_flutter_call_kit.dart';
 
 void initiateCall() {
   controlCall();
-  Random random = Random();
+  math.Random random = math.Random();
   CallEvent callEvent = CallEvent(
     sessionId: random.nextInt(100).toString(),
     callType: 1, // {0 :: Audio call}; {1 :: Video Call}
@@ -20,6 +24,9 @@ void initiateCall() {
   ConnectycubeFlutterCallKit.instance.updateConfig(ringtone: 'basi_sur', icon: 'app_icon', color: '#07711e');
   
   ConnectycubeFlutterCallKit.showCallNotification(callEvent);
+
+  ConnectycubeFlutterCallKit.onCallRejectedWhenTerminated = onCallRejectedWhenTerminated;
+ConnectycubeFlutterCallKit.onCallAcceptedWhenTerminated = onCallAcceptedWhenTerminated;
 }
 
 void controlCall() {
@@ -42,4 +49,47 @@ void controlCall() {
     onCallAccepted: _onCallAccepted,
     onCallRejected: _onCallRejected,
   );
+}
+Future<void> onCallAcceptedWhenTerminated(CallEvent event) async{
+  
+  print(
+      '[PushNotificationsManager][onCallAcceptedWhenTerminated] callEvent: $event');
+
+Future.delayed(Duration(seconds: 5,),(){
+    
+  navigatorKey?.currentState?.push(MaterialPageRoute(builder: (_) => CallingScreen()));
+  });
+
+  return sendPushAboutRejectFromKilledState({
+    'PARAM_CALL_TYPE': event.callType,
+    'PARAM_SESSION_ID': event.sessionId,
+    'PARAM_CALLER_ID': event.callerId,
+    'PARAM_CALLER_NAME': event.callerName,
+    'PARAM_CALL_OPPONENTS': event.opponentsIds.join(','),
+  }, event.callerId, event);
+}
+
+
+
+Future<void> onCallRejectedWhenTerminated(CallEvent callEvent) async {
+  print(
+      '[PushNotificationsManager][onCallRejectedWhenTerminated] callEvent: $callEvent');
+  return sendPushAboutRejectFromKilledState({
+    'PARAM_CALL_TYPE': callEvent.callType,
+    'PARAM_SESSION_ID': callEvent.sessionId,
+    'PARAM_CALLER_ID': callEvent.callerId,
+    'PARAM_CALLER_NAME': callEvent.callerName,
+    'PARAM_CALL_OPPONENTS': callEvent.opponentsIds.join(','),
+  }, callEvent.callerId, callEvent);
+}
+
+Future<void> sendPushAboutRejectFromKilledState(
+  Map<String, dynamic> parameters,
+  int callerId,
+  CallEvent callEvent
+) async{
+  CubeSettings.instance.applicationId = Config.APP_ID;
+  CubeSettings.instance.onSessionRestore = () {
+    return createSession(CubeUser(id: callerId));
+  };
 }
