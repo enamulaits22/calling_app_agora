af8aefb8ce9cb69362bb7adfebc1b017fc45176e
enamulaits22 <enamulaits22@gmail.com>
2022-07-21 13:29:53 +0600

    contact list done

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,7 +1,7 @@
 class Config {
   Config._();
   static String APP_ID = '46f98f952b5f4a608f651d43fdf47cd9';
-  static String Token = '00646f98f952b5f4a608f651d43fdf47cd9IACvrAsqvWAED/X9O79//hlkqnVzFCTz0xsmUTlZr+vvfXG6sh0AAAAAEABE3sOcTsjYYgEAAQBOyNhi';
+  static String Token = '00646f98f952b5f4a608f651d43fdf47cd9IADGYMqERGSH96cBvndGzQgyUnsAy95v9FBtNXIRJ1/XDnG6sh0AAAAAEABqn0DJ4VHaYgEAAQDhUdpi';
   static String channelName = 'audacitycall';
 
   /******************************* Sahred Preference Keys *******************************/
