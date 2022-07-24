db9c555a19d8d0e92a2618def0035a81248f7c69
rieadaits <rieadaits21@gmail.com>
2022-07-24 15:16:44 +0600

    bloc class added

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,7 +1,7 @@
 class Config {
   Config._();
   static String APP_ID = '46f98f952b5f4a608f651d43fdf47cd9';
-  static String Token = '00646f98f952b5f4a608f651d43fdf47cd9IADGYMqERGSH96cBvndGzQgyUnsAy95v9FBtNXIRJ1/XDnG6sh0AAAAAEABqn0DJ4VHaYgEAAQDhUdpi';
+  static String Token = '00646f98f952b5f4a608f651d43fdf47cd9IADJWEaFvoS0RK8Pygr+H+vro8UJerbq7PttbXWEh4n4TXG6sh0AAAAAEABsSV6NSxjeYgEAAQBLGN5i';
   static String channelName = 'audacitycall';
 
   /******************************* Sahred Preference Keys *******************************/
